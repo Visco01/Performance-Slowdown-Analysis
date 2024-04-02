a4215f842af59aa624fbf6e629e30c64f4cfba4c
Francesco Forcellato <francesco.forcellato@gmail.com>
2024-04-02 16:00:40 +0200

    Refactor of processor sharing

--- processor_sharing.py
+++ processor_sharing.py
@@ -3,15 +3,30 @@ import numpy as np
 from scipy.stats import truncpareto
 import matplotlib.pyplot as plt
 
-def PS_slowdown(rho):
-    return 1/(1-rho)
 
-fig, ax = plt.subplots(1, 1)
-max_y_value = 15
-alfa = 1.4
-maxv = 10000
-rho = 0.7
+class ProcessorSharing():
+    def __init__(self, alpha: float, max_v: int, rho: float) -> None:
+        self.alpha = alpha
+        self.max_v = max_v
+        self.rho = rho
 
-x_values = np.linspace(1,20,100)
+    def slowdown(self):
+        return 1/(1-self.rho)
 
-PS_slowdown = np.full_like(x_values, PS_slowdown(0.7))
+    def get_slowdowns(self, x_values: list[float]) -> list[float]:
+        return np.full_like(x_values, self.slowdown())
+
+
+# def PS_slowdown(rho):
+#     return 1/(1-rho)
+
+
+# fig, ax = plt.subplots(1, 1)
+# max_y_value = 15
+# alfa = 1.4
+# maxv = 10000
+# rho = 0.7
+
+# x_values = np.linspace(1, 20, 100)
+
+# PS_slowdown = np.full_like(x_values, PS_slowdown(0.7))
