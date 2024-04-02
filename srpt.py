5374d24f78ac2cf90f6b3e691e56edf989069748
Visco01 <visconti373@gmail.com>
2024-04-02 13:54:09 +0200

    Fix SRPT slowdown

--- srpt.py
+++ srpt.py
@@ -4,55 +4,44 @@ from scipy.stats import truncpareto
 import matplotlib.pyplot as plt
 import math
 
-def calc_rho(x, l, alfa, maxv):
-    ft = lambda t: t*truncpareto.pdf(t,alfa,maxv)
-    integral= quad(ft, 1, x)
-    return integral[0]*l
-
-def m2(x, l, alfa, maxv):
-    ft = lambda t: (t**2)*truncpareto.pdf(t,alfa,maxv)
-    integral= quad(ft, 1, x)
-    return integral[0]*l
-
-def service_time(x, alfa, maxv):
-    ft = lambda t: t*truncpareto.pdf(t,alfa,maxv)
-    integral= quad(ft, 1, np.inf)
-    return integral[0]
-
-def last_integral(x, l, alfa, maxv):
-    ft = lambda t: 1/(1-calc_rho(x, l, alfa, maxv))
-    integral= quad(ft, 1, x)
-    return integral[0]
-
-def srpt_response_time(x, l, alfa, maxv):
-    numerator = l*(m2(x, l, alfa, maxv)+(x**2)*(1-truncpareto.cdf(x, alfa, maxv)))
-    denominator = 2*(1-calc_rho(x, l, alfa, maxv))**2
-    return (numerator/denominator)+last_integral(x, l, alfa, maxv)
-
-def slowdown(rho, l, mu, sigma, job_size):
-    return srpt_response_time(job_size, l, alfa, maxv)/job_size
-
-fig, ax = plt.subplots(1, 1)
-max_y_value = 15
 alfa = 1.4
 maxv = 10000
+tft = lambda t: t * truncpareto.pdf(t, alfa, maxv)
+ttft = lambda t: t * tft(t)
+cdf = lambda x: truncpareto.cdf(x, alfa, maxv)
+rho_x = lambda x: quad(tft, 1, x)[0] * l
+last_integral_x = lambda x: 1/(1-rho_x(x))
+
 rho = 0.7
-mu = truncpareto.mean(alfa, maxv)
+mu = 1 / quad(tft, 0, np.inf)[0]
 l = rho * mu
 sigma = math.sqrt(truncpareto.var(alfa, maxv))
 
-# ax.set_yscale('log')
-# ax.set_xscale('log')
+
+def m2(x):
+    integral = quad(ttft, 1, x)
+    return integral[0] * l
+
+def last_integral(x):
+    integral = quad(last_integral_x, 1, x)
+    return integral[0]
+
+def srpt_response_time(x):
+    numerator = l * (m2(x) + (x**2) * (1 - cdf(x)))
+    denominator = 2 * (1 - rho_x(x))**2
+    return (numerator / denominator) + last_integral(x)
+
+def srpt_slowdown(x):
+    return srpt_response_time(x) / x
+
+
+fig, ax = plt.subplots(1, 1)
 x_values = np.linspace(1,20,100)
 
 y_SRPT_values = []
 for x in x_values:
-    y_SRPT_values.append(slowdown(0.7, l, mu, sigma, x))
+    y_SRPT_values.append(srpt_slowdown(x))
 
 ax.plot(x_values, y_SRPT_values, 'b-', lw=2, alpha=0.6, label='SRPT_slowdown')
 
 plt.show()
-
-print(calc_rho(10, l, alfa, maxv))
-print(service_time(2, alfa, maxv))
-print(m2(10, l, alfa, maxv))
